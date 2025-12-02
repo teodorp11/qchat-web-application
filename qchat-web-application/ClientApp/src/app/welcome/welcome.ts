199cfc4868d32f8c90c48dc260f800ff0a558977
teodorp11 <teodor.posea@gmail.com>
2025-12-02 14:54:33 +0200

    feat(welcome): Add 'Welcome' page

--- qchat-web-application/ClientApp/src/app/welcome/welcome.ts
+++ qchat-web-application/ClientApp/src/app/welcome/welcome.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './welcome.html',
   styleUrl: './welcome.css',
 })
-export class Welcome {
-
-}
+export class Welcome {}
